87815ef4b764519cb2d2b8598bea7b1e802bcdfe
NG H <angelcemept@gmail.com>
2024-05-11 16:07:06 +0800

    upload game core scripts

---
+++ GameCore_v1_CS_project/PlayerObj.cs
@@ -0,0 +1,26 @@
+﻿
+/*
+
+ This script is players class
+
+ */
+
+namespace GameCore_v1
+{
+    internal class PlayerObj
+    {
+        public string Name { get; set; }
+
+        public int N_piece { get; set; }
+
+        public int piece_on_board { get; set; } 
+
+        public char color { get; set; }
+
+        public PlayerObj(string name, int number_piece)
+        {
+            Name = name;
+            N_piece = number_piece;
+        }
+    }
+}
